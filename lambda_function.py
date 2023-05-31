1853afbc2da681e149ca97529e35da04a60a442b
Mauricio Quevedo <mauriqg@gmail.com>
2023-05-31 19:28:28 -0400

    update

--- lambda_function.py
+++ lambda_function.py
@@ -1,49 +1,56 @@
 import json
 import boto3
+
 def lambda_handler(event, context):
-    #showID = next(iter(event.keys()))
-    # Retrieve the email data from the event object
-    #email = event[showID]['email']
-    # Create a DynamoDB client
+    # Retrieve the email and event data from the event object
+    email = event['body']['email']
+    eventData = event['body']['eventData']
     
+    # Create a DynamoDB client
     dynamodb = boto3.client('dynamodb')
- 
-    # Write the email to the DynamoDB table
-    response = dynamodb.put_item(
-        TableName='funtubeDB2',
-        Item={
-        'id': {'S': event['id']},
-        'email': {'S': event['payload']['email']},
-        'name': {'S': event['payload']['eventData']['name']},
-        'location': {'S': event['payload']['eventData']['location']},
-        'date': {'S': event['payload']['eventData']['date']},
-        'time': {'S': event['payload']['eventData']['time']},
-        'min': {'S': event['payload']['eventData']['min']},
-        'max': {'S': event['payload']['eventData']['max']}
-                }
-            )
+    
+    # Write the email and event data to the DynamoDB table
+    try:
+        response = dynamodb.put_item(
+            TableName='RegisteredEmails',
+            Item={
+                'email': {'S': email},
+                'eventData': {'S': json.dumps(eventData)}
+            }
+        )
+    except Exception as e:
+        # Handle DynamoDB write errors
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error writing email and event data to DynamoDB: {}'.format(str(e)))
+        }
+    
     # Subscribe the user's email to an SNS topic
     sns = boto3.client('sns')
+    
     # Replace the sns_topic_arn with your SNS topic ARN
-    sns_topic_arn = 'arn:aws:sns:us-east-1:035082996281:email'
+    sns_topic_arn = 'arn:aws:sns:us-east-1:748795926248:emailsubtest'
+    
     try:
         response = sns.subscribe(
             TopicArn=sns_topic_arn,
             Protocol='email',
-            Endpoint=event['payload']['email']
+            Endpoint=email
         )
-        # Customize the notification message here
-        notification_message = 'Thank you for subscribing to Funtube, we will keep you updated on you selected events price.'
+        
+        # Customize the notification message with the selected event data
+        notification_message = f'Thank you for subscribing to Funtube. You have selected the following event:\n\nName: {eventData[0]}\nLocation: {eventData[1]}\nVenue: {eventData[2]}\nDate: {eventData[3]}\nTime: {eventData[4]}\nPrice: {eventData[5]}'
+        
         # Send the customized notification to the user
         response = sns.publish(
             TopicArn=sns_topic_arn,
             Message=notification_message
         )
-
-        # Return a response
+        
+        # Return a successful response
         return {
             'statusCode': 200,
-            'body': json.dumps('Email registered successfully and user subscribed to notifications')
+            'body': json.dumps('Email registered successfully, and user subscribed to notifications with the selected event')
         }
     except Exception as e:
         # Handle any errors that occur during the subscription process
@@ -51,21 +58,3 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps('Error subscribing user to notifications: {}'.format(str(e)))
         }
-
-
-#Configure Test Event
-# {
-#   "id": "2342543",
-#   "payload": {
-#     "email": "testemail4@test.com",
-#     "eventData": {
-#       "name": "Taylor Swift",
-#       "location": "Atlanta",
-#       "venue": "here",
-#       "date": "2023-05-19",
-#       "time": "18:30:00",
-#       "min": "9",
-#       "max": "10"
-#     }
-#   }
-# }
